a68efc95770042cb7d2dba88de7a72564c8a0d97
olsanche <olsanche@outlook.com>
2013-04-12 20:33:42 -0400

    Ajout du chapeau et amélioration de la detection

--- oliPotter_simplecv.py
+++ oliPotter_simplecv.py
@@ -11,10 +11,10 @@ def main():
     imHat=imHat.resize(200,200)                                         #Dimension de l'image
     amask=imHat.colorDistance(color=Color.WHITE).binarize().invert()    # Creationu mask pour supprimer le controur blanc
 
+    wizard_hat=Image("wizard_hat.png")
     # Image du lapin
     imRabbit = Image('C:\Users\Olivier\Pictures\hat-magic-rabbit.gif')
     imRabbit=imRabbit.resize(200,200)
-
     # Creation du mask pour supprimer le fond vert
     amaskg=imRabbit.colorDistance(color=Color.GREEN).binarize().invert()
 
@@ -22,7 +22,7 @@ def main():
     pixColor=Color.RED
 
     done = False # setup boolean to stop the program
-    thresh=50
+    thresh=35
 
     # Boucle principalw
     while not display.isDone():
@@ -45,11 +45,22 @@ def main():
                             thresh+=1
                             print "Thresh = "+repr(thresh)
 
+            # Detection du visage
+            faces = img.findHaarFeatures("face") # load in trained face file
+            if faces:
+                face = faces[-1]
+                imgWizard= wizard_hat.resize(face.width()+200,face.height()+200) #load the image to super impose and scale it correctly
+                mymask = imgWizard.colorDistance(color=Color.WHITE)
+
+                # Place le chapeau
+                posWizard=(face.topLeftCorner()[0]-100,face.topLeftCorner()[1]-imgWizard.size()[1]+40)
+                img = img.blit(imgWizard, posWizard,alphaMask=mymask)
+
             # Traitement de l'image source pour r?cup?rer le blob de l'objet recherch?
             # A partir de l'image source, on cr?e une morphologie de type "OPEN"
             # Ensuite on r?cup?re l'ensemble des pixels correspondant ? la couleur recherch?
             # puis on cr?e l'image binaire avec le Threshold
-            img2=img.morphOpen().colorDistance(pixColor).binarize(thresh)
+            img2=img.morphOpen().hueDistance(pixColor).binarize(35)
 
             # Recup?rtion de tous les blobs
             blobs = img2.findBlobs()
@@ -64,9 +75,9 @@ def main():
                             if b.area()>500:
                                     b.draw(color=Color.PUCE, width=3)
                     #img.sideBySide(img2)
-                                    img.addDrawingLayer(img2.dl())
-                                    img.dl().circle(b.centroid(),10,Color.RED)
-                                    img.dl().text("Position: "+repr(b.centroid()),(0,10),Color.GREEN)
+                                    #img.addDrawingLayer(img2.dl())
+                                    #img.dl().circle(b.centroid(),10,Color.RED)
+                                    #img.dl().text("Position: "+repr(b.centroid()),(0,10),Color.GREEN)
                                     if b.centroid()[0]>x and b.centroid()[0]<x+imHat.size()[0] and b.centroid()[1]>img.size()[1]-imHat.size()[1]:
                                         img=img.blit(imRabbit,pos=(img.size()[0]/2-imRabbit.size()[0]/2,img.size()[1]-imRabbit.size()[1]-150),alphaMask=amaskg)
 
